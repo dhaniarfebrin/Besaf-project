90524f4e3d5e8ce129a9fae8f1c61475cbdfee53
nickname9909 <jomblokronis88@gmail.com>
2020-02-03 15:40:00 +0700

    menambah halaman tamu yang mana bisa melihat turnamen.. di sini masih ada yang kurang yaitu modal2

--- application/views/Front/template/header.php
+++ application/views/Front/template/header.php
@@ -5,10 +5,11 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Besaf</title>
+	<title>BESAF</title>
 	<link rel="stylesheet" href="<?= base_url() ?>assets/Front/css/bootstrap.css">
 	<link rel="stylesheet" href="<?= base_url() ?>assets/Front/css/all.css">
 	<link rel="stylesheet" href="<?= base_url() ?>assets/Front/css/Style.css">
+	<link rel="shortcut icon" href="favicon.ico">
 </head>
 
 <body>
