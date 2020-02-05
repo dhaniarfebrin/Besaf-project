d42155a1662edb4fe9f4fe79c24db3ec7ff3c90d
Kalwabed Rizki <49640654+kaliwa@users.noreply.github.com>
2020-02-05 14:26:57 +0700

    Update tournamen user (#4)
    * update database
    
    * tournament
    
    * update database
    
    kembali ke besaf
    
    Co-authored-by: Alfiansyah <60593001+iansyah724@users.noreply.github.com>

--- application/views/tournament_view.php
+++ application/views/tournament_view.php
@@ -20,79 +20,22 @@
 			</div>
 		</div>
 		<div class="m-portlet__body">
-			<div class="row mt-3">
-				<?php
-				for ($no=2; $no <= 4; $no++) {
-				?>
-				<div class="col-sm-4">
-					<a href="<?php echo base_url('turnamen') ?>">
-						<div class="m-portlet m-portlet--mobile">
-							<img class="hover" src="<?php echo base_url('assets/app/media/img/products/product').$no.".jpg" ?>" alt="" style="width: 100%" class="rounded border">
-						</div>
-					</a>
-				</div>
-				<?php } ?>
+			<div class="row mt-3 turnamen-terlaris">
 			</div>
 		</div>
 	</div>
 	<div class="col-md-3">
-		<select name="" class="form-control">
+		<select class="form-control jadwal">
 			<option value="">All</option>
-			<option value="">Belum Tersedia</option>
+			<option value="1">Sedang berlangsung</option>
+			<option value="2">Selesai</option>
 		</select>
 	</div>
-	<div class="mt-3">
-		<?php  
-		for ($no=2; $no <= 9; $no++) { 
-		?>
-		<a href="<?php echo base_url('turnamen') ?>" style="text-decoration: none">
-			<div class="m-portlet m-portlet--mobile hover">
-				<div class="row">
-					<div class="col-md-3">
-						<div style="padding: 5%; margin-top: 2px">
-							<img src="<?php echo base_url('assets/app/media/img/products/product').$no.".jpg" ?>" style="width: 100%" class="rounded border">
-						</div>	
-					</div>
-					<div class="col-md-9">
-						<div class="m-portlet__head">
-							<div class="m-portlet__head-caption">
-								<div class="m-portlet__head-title">
-									<h3 class="m-portlet__head-text">
-										Product <?php echo ($no-1); ?>
-									</h3>
-								</div>
-							</div>
-						</div>
-						<div class="m-portlet__body">
-							<div class="row">
-								<div class="col-sm-2">
-									ENTRY
-									<br><b>Free</b>
-								</div>
-								<div class="col-sm-2">
-									SLOTS
-									<br><b><?php echo ($no*2)."/32"; ?></b>
-								</div>
-								<div class="col-sm-2">
-									TIME 
-									<br><b><?php echo $tgl = date('Y-m-d'); ?></b>
-								</div>
-								<div class="col-sm-2">
-									CLOSE IN 
-									<br><?php echo date('Y-m-d',strtotime('+3 days',strtotime($tgl))) ?>
-								</div>
-								<div class="col-sm-3" align="right">
-									Cookies
-									<br><?php echo ($no*3)."00"; ?>
-								</div>
-									<i class="fa fa-cookie-bite text-warning" style="font-size: 30px"></i>
-								<!-- </div> -->
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</a>
-		<?php } ?>
+	<div class="mt-3 all-turnamen">
+		<div align="center">
+			<div class="m-spinner m-spinner--danger m-spinner--lg"></div>
+			<div class="m-spinner m-spinner--warning m-spinner--lg"></div>
+			<div class="m-spinner m-spinner--success m-spinner--lg"></div>
+		</div>
 	</div>
 </div>
--- application/views/turnamen/view-1.php
+++ application/views/turnamen/view-1.php
@@ -15,43 +15,25 @@
 		<div class="row">
 			<div class="col-md-12" align="center">
 				<div class="m-portlet m-portlet--mobile">
-					<div class="m-portlet__body">
-						<img src="<?php echo base_url('assets/app/media/img/products/product1.jpg'); ?>" alt="Gambar" style="height: 300px; max-width: 100%">
+					<div class="m-portlet__body foto">
+						<img src="aaaa" alt="memuat...">
 					</div>
 				</div>
 			</div>
 			<div class="col-md-5 mt-3">
 				<div class="m-portlet m-portlet--mobile">
-					<div class="m-portlet__body">
-						<img src="<?php echo base_url('assets/app/media/img/products/product4.jpg'); ?>" class="card-img-top" alt="Hiyaaaaa" style="width: 100%">
-						<div class="card-body">
-							<div class="badge badge-danger" style="padding: 10px">Featured</div>
-							<div class="badge badge-success" style="padding: 10px">Featured</div>
-							<hr>
-							<h3>Tournament Gajelas</h3>
-							<small><?php echo date('d m Y'); ?> s/d <?php echo date('d m Y',strtotime('+3 day')); ?></small><br>
-							<big><strong>Squad Kulu-kulu</strong></big>
-							<hr>
-							<table class="table table-sm">
-								<tr>
-									<th>Hosted by</th>
-									<td>Mancing mania</td>
-								</tr>
-								<tr>
-									<th>Community</th>
-									<td>Banyak</td>
-								</tr>
-								<tr>
-									<th>Tournament mode</th>
-									<td>Battle royal</td>
-								</tr>
-								<tr>
-									<th>Venue</th>
-									<td>Pindah2</td>
-								</tr>
-							</table>
-						</div>					
+					<div class="m-portlet__body keterangan">
 					</div>
+				</div>
+				<div class="m-portlet m-portlet--mobile">
+					<button class="btn btn-block btn-light" data-toggle="modal" data-target="#aturan">
+						<div align="center"><i class="flaticon-list-1"></i> Aturan-aturan</div>
+					</button>
+				</div>
+				<div class="m-portlet m-portlet--mobile">
+					<button class="btn btn-block btn-light" data-toggle="modal" data-target="#cara">
+						<div align="center"><i class="flaticon-info"></i> Cara mengikuti tournament</div>
+					</button>
 				</div>
 			</div>
 			<div class="col-md-7 mt-3">
@@ -67,8 +49,8 @@
 					</div>
 					<div class="m-portlet__body">
 						<div class="row">
-							<div class="col">
-								<h1>Rp. 1.000.000</h1>
+							<div class="col hadiah">
+
 							</div>
 							<div class="col">
 								<div align="right">
@@ -89,21 +71,11 @@
 						</div>
 					</div>
 					<div class="m-portlet__body">
-						<p>Tournament yang diselenggarakan oleh Yamisok bekerjasama dengan iCafe / Warnet se-Indonesia. Untuk kali ini Game yang akan dipertandingkan adalah PUBG LITE.</p>
+						<p>Tournament yang diselenggarakan oleh Yamisok bekerjasama dengan iCafe / Warnet se-Indonesia. Untuk kali ini Game yang akan dipertandingkan adalah <b class="game_nama">Nama Game</b>.</p>
 						<p>Untuk lebih jelasnya silahkan check dibagian RULES tournament. Slot terbatas..!! Segera daftarkan dirimu jangan sampai kehabisan slot..</p>
 						<br>
 						<strong>GOOD LUCK !</strong>
 					</div>
-				</div>
-				<div class="m-portlet m-portlet--mobile">
-					<button class="btn btn-block btn-light" data-toggle="modal" data-target="#aturan">
-						<div align="center"><i class="flaticon-list-1"></i> Aturan-aturan</div>
-					</button>
-				</div>
-				<div class="m-portlet m-portlet--mobile">
-					<button class="btn btn-block btn-light" data-toggle="modal" data-target="#cara">
-						<div align="center"><i class="flaticon-info"></i> Cara mengikuti tournament</div>
-					</button>
 				</div>
 			</div>
 		</div>
@@ -150,7 +122,7 @@
 				<button class="close" data-dismiss="modal" type="button">x</button>
 			</div>
 			<div class="modal-body">
-				<strong>KEPUTUSAN PANITIA SUDAH MUTLAK</strong>
+				<p class="aturan"></p>
 			</div>
 		</div>
 	</div>
@@ -164,7 +136,7 @@
 				<button class="close" data-dismiss="modal" type="button">x</button>
 			</div>
 			<div class="modal-body">
-				Belum Tersedia
+				<p class="how-join"></p>
 			</div>
 		</div>
 	</div>
